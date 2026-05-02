70a03230d62c92684223e4e1e972e32dcc482391
Mithran2003 <mithransaini5@gmail.com>
2026-05-03 02:29:28 +0530

    added finete ammount of food

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -9,11 +9,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject projectilePrefab ;
     [SerializeField]
+    private int startingFoodCount ;
+    private int currentFoodCount ;
+    [SerializeField]
     private float maxX = 24 ; // Variable to set the Maximum value of X cordinate  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentFoodCount = startingFoodCount;
+        Debug.Log($"Number of apples on player is :{currentFoodCount}");
     }
 
     // Update is called once per frame
@@ -30,10 +34,16 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = (new Vector3( -maxX , transform.position.y, transform.position.z ));
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.Space)) && (currentFoodCount>0))
         {
              Instantiate(projectilePrefab /*game object*/,transform.position /* player coordinates */,projectilePrefab.transform.rotation/* game object rotation */);
+             currentFoodCount -= 1;
+             Debug.Log($"Number of apples left on the player {currentFoodCount} started with {startingFoodCount} number of apples ");
             
+        }
+        if ((Input.GetKeyDown(KeyCode.Space)) && (currentFoodCount<=0))
+        {
+            Debug.Log("NO more apples to feed try to find more apples  ");
         }
         
 
--- Assets/Course Library/Scripts/SpawnManager.cs
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -1,18 +1,44 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] animalPrefab ;
+    private  float xSpwanRange ;
+    private float  zSpwanRange ;
+    private Vector3 spawnPoint ;
+    private short animalIndex ;
+    private float waitFor;
+    [SerializeField]
+    private bool playerIsAlive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(randomSpwaner());
     }
+    IEnumerator randomSpwaner ()
+    {
+        while(playerIsAlive) 
+        {
+            yield return new WaitForSeconds(waitFor);
+            Instantiate(animalPrefab[animalIndex],spawnPoint,animalPrefab[animalIndex].transform.rotation);
+        }
+            
+        }
+        
+       
 
     // Update is called once per frame
     void Update()
     {
+        xSpwanRange = UnityEngine.Random.Range(-23,23);
+        zSpwanRange = UnityEngine.Random.Range(18,30);
+        spawnPoint = new Vector3(xSpwanRange,0f,zSpwanRange);
+        waitFor = UnityEngine.Random.Range(0,4);
+        animalIndex = Convert.ToInt16(UnityEngine.Random.Range(0,4));
         
     }
+    
 }
